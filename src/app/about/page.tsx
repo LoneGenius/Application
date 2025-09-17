dd6ae79f3c20e6085ed4e20de6a2b4bb234c377e
LoneGenius <nurulnajwanorhisham02@gmail.com>
2025-09-18 00:02:19 +0800

    fix title

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,7 @@
 import AboutContent from './AboutContent';
 
 export const metadata = {
-  title: 'About Us | TechnologyKU',
+  title: 'About Us | TechnologyKu',
   description: 'Who we are and why we built TechnologyKU',
 };
 
